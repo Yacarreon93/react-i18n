4261450f23c008b7610906a82692453104fa208c
Yasser Carreón <yacasia@gmail.com>
2018-07-03 10:10:21 -0500

    Use templates

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ class App extends Component {
            <FormattedMessage id="detail.reviewsHeading"/>
           </h1>
         </header>
-        <p>
-          <FormattedMessage id="detail.purchase"/>
-        </p>
+        <p><FormattedMessage id="detail.author" values={{ author: <b>Yasser</b> }}/></p>
+        <p><FormattedMessage id="detail.averageRating" values={{ avg: '10' }}/></p>
+        <p><FormattedMessage id="detail.purchase"/></p>
         <button>
           <FormattedMessage id="detail.toggle"/>
         </button>
--- src/messages.js
+++ src/messages.js
@@ -1,23 +1,29 @@
 export default {
   'en-US': {
-  detail: {
-    toggle: 'Toggle',
-    purchase: 'Purchase this book from:',
-    reviewsHeading: 'Reviews',
-  }
-},
-'es-MX': {
-  detail: {
+    detail: {    
+      author: 'by {author}',
+      toggle: 'Toggle',
+      purchase: 'Purchase this book from:',
+      reviewsHeading: 'Reviews',
+      averageRating: 'Average Rating: {avg}',
+    }
+  },
+  'es-MX': {
+    detail: {
+      author: 'de {author}',
       toggle: 'Palanca',
       purchase: 'Compre este libro de:',
       reviewsHeading: 'Comentarios',
-  }
-},
-'fr-FR': {
+      averageRating: 'Puntuación Media: {avg}',
+    }
+  },
+  'fr-FR': {
     detail: {
+      author: 'par {author}',
       toggle: 'Basculer',
       purchase: 'Echetez ce livre à partir de:',
       reviewsHeading: 'Avis',
+      averageRating: 'Note moyenne: {avg}',
     }
   },
 };
